fd8827381baf97e5f450876f761f7e663b3efb41
dharanidharansr <samidharani07@gmail.com>
2026-08-25 13:05:36 +0000

    babu's second fault

--- crm/crm/doctype/opportunity/opportunity.py
+++ crm/crm/doctype/opportunity/opportunity.py
@@ -1,21 +1,22 @@
 import frappe
+from frappe import _
 from frappe.model.document import Document
 
 
 class Opportunity(Document):
 	def validate(self):
 		if not self.lead:
-			frappe.throw("Lead is required.")
+			frappe.throw(_("Lead is required."))
 
 		lead_status = frappe.db.get_value("Lead", self.lead, "status")
 
 		if lead_status != "Qualified":
-			frappe.throw("Only Qualified Leads can be converted into an Opportunity.")
+			frappe.throw(_("Only Qualified Leads can be converted into an Opportunity."))
 
 		if self.opportunity_value < 0:
-			frappe.throw("Opportunity Value cannot be negative.")
+			frappe.throw(_("Opportunity Value cannot be negative."))
 
 		probability = self.probability or 0
 
 		if not 0 <= probability <= 100:
-			frappe.throw("Probability must be between 0 and 100.")
+			frappe.throw(_("Probability must be between 0 and 100."))
