a3926612a67c641326cac35b52ba1466d704d345
dharanidharansr <samidharani07@gmail.com>
2026-08-25 12:49:19 +0000

    format: solved eslint

--- crm/crm/doctype/opportunity/opportunity.py
+++ crm/crm/doctype/opportunity/opportunity.py
@@ -1,9 +1,21 @@
-# Copyright (c) 2026, Team Aruvi and contributors
-# For license information, please see license.txt
-
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class Opportunity(Document):
-	pass
+	def validate(self):
+		if not self.lead:
+			frappe.throw("Lead is required.")
+
+		lead_status = frappe.db.get_value("Lead", self.lead, "status")
+
+		if lead_status != "Qualified":
+			frappe.throw("Only Qualified Leads can be converted into an Opportunity.")
+
+		if self.opportunity_value < 0:
+			frappe.throw("Opportunity Value cannot be negative.")
+
+		probability = self.probability or 0
+
+		if not 0 <= probability <= 100:
+			frappe.throw("Probability must be between 0 and 100.")
