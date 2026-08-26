2cfc9bac57056f25840af9462f04a9126da78f9e
Sri Ragul <sriragul.aa@gmail.com>
2026-08-26 06:04:14 +0000

    feat: added lead qualification part

--- crm/crm/doctype/lead/lead.js
+++ crm/crm/doctype/lead/lead.js
@@ -6,3 +6,23 @@
 
 // 	},
 // });
+
+frappe.ui.form.on("Lead", {
+	status(frm) {
+		if (frm.doc.status === "Qualified" && frm.doc.requirement) {
+			frm.set_value("qualification_date", frappe.datetime.get_today());
+		}
+	},
+
+	validate(frm) {
+		if (frm.doc.status === "Qualified") {
+			if (!frm.doc.requirement) {
+				frappe.throw("Please enter the Lead Requirement before qualifying this Lead.");
+			}
+
+			if (!frm.doc.qualification_date) {
+				frm.set_value("qualification_date", frappe.datetime.get_today());
+			}
+		}
+	},
+});
