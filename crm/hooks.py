527bab827db98defd325ee72731acd26d6482591
dharanidharansr <samidharani07@gmail.com>
2026-08-26 05:45:40 +0000

    feat: fully fixed fixture

--- crm/hooks.py
+++ crm/hooks.py
@@ -7,10 +7,7 @@ app_license = "mit"
 
 # Apps
 # ------------------
-fixtures = [
-	{"dt": "Client Script", "filters": [["name", "=", "Opportunity Client Script"]]},
-	{"dt": "Client Script", "filters": [["name", "=", "Follow Up Script"]]},
-]
+fixtures = [{"dt": "Client Script", "filters": [["name", "in", ["Probability", "Follow Up Script"]]]}]
 # required_apps = []
 
 # Each item in the list will be shown as an app in the apps page
